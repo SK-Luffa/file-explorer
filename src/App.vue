6152c5151ea76bb405e6aa041e85889a94581a4b
dengzixu <sk8sigua@aliyun.com>
2023-02-26 16:45:33 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import NavBar from "./components/NavBar/index.vue"
+</script>
+
+<template>
+  <div class="container">
+    <header class="header">
+      <NavBar></NavBar>
+    </header>
+    <div class="wrapper">
+      <div class="side"></div>
+      <div class="content"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="less">
+.container {
+  width: 100%;
+  height: 100vh;
+  background-color: var(--color-background);
+  .header {
+    background-color: var(--color-background-mute);
+  }
+  .wrapper {
+    display: flex;
+    width: 100%;
+    height: 100%;
+
+    .side {
+      width: 200px;
+      height: 100%;
+      background-color: var(--black-soft);
+    }
+    .content {
+      flex: 1 1 auto;
+      height: 100%;
+    }
+  }
+}
+</style>
---
+++ src/components/NavBar/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="nav-bar-container">
+    <div class="title">资源管理器</div>
+    <div class="options">操作区</div>
+  </nav>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="less" scoped>
+.nav-bar-container {
+  width: 100%;
+  height: 28px;
+  line-height: 28px;
+  display: flex;
+  color: var(--color-text);
+  font-size: 14px;
+  padding: 0 8px;
+  .title {
+    cursor: pointer;
+  }
+  .options {
+    margin-left: 24px;
+  }
+}
+</style>
